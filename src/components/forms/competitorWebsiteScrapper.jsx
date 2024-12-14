285e246a03562710738e7529739b0dc26779c887
Qaim-Prystech <qaimprystech@gmail.com>
2024-12-14 09:51:52 +0500

    Code Cleaning

---
+++ src/components/forms/competitorWebsiteScrapper.jsx
@@ -0,0 +1,138 @@
+import * as yup from "yup";
+import { useForm, FormProvider, } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
+import SecondStep from "./secondStep";
+import RHFTextfield from "../helpers/RHFTextfield";
+import TagInput from "../helpers/TagInput";
+
+export default function CompetitorWebsiteScrapperForm() {
+  const [tab, setTab] = useState(0);
+  const [urls, setUrls] = useState([]);
+
+  function handleTabChange(tab) {
+    setTab(tab)
+  }
+
+  const formSchema = yup.object({
+    url: yup.array().of(yup.string().url("Invalid URL").required("Each URL must be valid")).min(1, "At least one URL is required").required("At least one URL is required"), // Ensures the URL array is not empty
+    email: yup.string().email("Please enter a valid email address.").required("Email is required"),
+    goal: tab === 1 ? yup.array().of(yup.string().required("Goal is required")).min(1, "At least one goal must be selected").transform((value, originalValue) => {
+      if (originalValue === "") {
+        return [];
+      }
+      return value;
+    }) : yup.string(),
+    company: tab === 1 ? yup.string().required("Company name is required") : yup.string(),
+    name: tab === 1 ? yup.string().required("Name is required") : yup.string(),
+    revenue: tab === 1 ? yup.number().nullable().transform((value, originalValue) => originalValue === "" ? null : value).typeError("Input must be a number").required("Revenue amount is required") : yup.string(),
+    budget: tab === 1 ? yup.string().required("Growth budget is required") : yup.string(),
+  });
+  const methods = useForm({
+    resolver: yupResolver(formSchema),
+    defaultValues: {
+      url: [],
+      email: "",
+      goal: "",
+      company: "",
+      name: "",
+      revenue: "",
+      budget: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    if (data.url.length === 0) {
+      methods.setError("url", { message: "At least one URL is required." });
+      return;
+    }
+    console.log(data);
+    // If validation passes, proceed with form submission
+    switch (tab) {
+      case 0:
+        const step1Object = {
+          email: data.email,
+          compURLs: data.url,
+          name: null,
+          company: null,
+          growthGoal: null,
+          revenueGoal: null,
+          estGrowthBudget: null
+        }
+        sendToZapier('https://hooks.zapier.com/hooks/catch/356942/2sk0oqt/', step1Object, 1, setTab)
+
+        break;
+      case 1:
+        const step2Object = {
+          email: data.email,
+          compURLs: data.url,
+          name: data.name,
+          company: data.company,
+          growthGoal: data.goal,
+          revenueGoal: data.revenue,
+          estGrowthBudget: data.budget
+        }
+        sendToZapier('https://hooks.zapier.com/hooks/catch/356942/2sk0oqt/', step2Object, 2, setTab)
+        break;
+      default:
+        break;
+    }
+  };
+
+  const selectItems = [
+    { value: "Drive more high-quality organic traffic", label: "Drive more high-quality organic traffic" },
+    { value: "Reduce CAC and paid ad dependency", label: "Reduce CAC and paid ad dependency" },
+    { value: "Build strong pipeline of high-quality leads", label: "Build strong pipeline of high-quality leads" },
+    { value: "Identify demand to drive opportunities from new market", label: "Identify demand to drive opportunities from new market" },
+    { value: "Other", label: "Other" },
+  ];
+
+  return (
+    <section className="form-1">
+      <div className="inner">
+        <div className="form-wrap">
+          <FormProvider {...methods}>
+            <form className="form" onSubmit={methods.handleSubmit(onSubmit)}>
+              {tab === 0 && (
+                <>
+                  <TagInput
+                    onTagsChange={(newTags) => {
+                      setUrls(newTags);
+                      methods.setValue("url", newTags, { shouldValidate: true, shouldFocusError: true }); // Trigger validation and focus error
+                    }}
+                    className="input"
+                    name="url"
+                    label="Enter Competitor URLs"
+                    error={methods.formState.errors.url} // Pass the error here
+                  />
+                  <RHFTextfield isEmailField={true} className="input email-input" name="email" placeholder="Your Email" label="Enter Your Email Address" onBlur={() => methods.trigger('email')} />
+                  <div className="btn-wrap">
+                    <button type="submit" className="submit-btn" >
+                      <svg width="100%" height="100%" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.92602 20.6699C8.38277 20.6699 7.83953 20.4661 7.41065 20.0295L3.12187 15.6635C2.29271 14.8194 2.29271 13.4223 3.12187 12.5782C3.95104 11.7341 5.32345 11.7341 6.15261 12.5782L8.92602 15.4016L18.8474 5.30153C19.6766 4.45743 21.049 4.45743 21.8781 5.30153C22.7073 6.14562 22.7073 7.54274 21.8781 8.38684L10.4414 20.0295C10.0411 20.4661 9.46927 20.6699 8.92602 20.6699Z" fill="currentColor"></path>
+                      </svg>
+                      Get Free Alerts
+                    </button>
+                  </div>
+                </>
+              )}
+              {tab === 1 && (
+                <>
+                  <SecondStep
+                    selectItems={selectItems}
+                    onSubmit={methods.handleSubmit(onSubmit)}
+                  />
+                </>
+              )}
+              {tab === 2 && (
+                <>
+                  <iframe src="https://calendly.com/boringmarketing/boringmarketing-com-30min-demo-call " height={1000}></iframe>
+                </>
+              )}
+            </form>
+          </FormProvider>
+        </div>
+      </div>
+    </section >
+  );
+}
--- src/components/helpers/RHFSelect.jsx
+++ src/components/helpers/RHFSelect.jsx
@@ -1,39 +1,47 @@
-/*
 import { useFormContext, Controller } from "react-hook-form";
-    import Select from 'react-select';
+import Select from 'react-select';
 
-    export default function RHFSelect({ name, className, options, placeholder, label }) {
-        const {
-            control,
-            formState: { errors },
-        } = useFormContext(); // Access register and errors from the form context
-
-        return (
-            <>
-                <div className="form-group">
-                    <label className="input-label" for={name}>{label}</label>
-                    <Controller
-                        name={name}
-                        control={control}
-                        rules={{ required: "Goal is required" }}
-                        render={({ field }) => (
-                            <Select
-                                isMulti
-                                {...field}
-                                className={className}
-                                options={options}
-                                placeholder={placeholder}
-                                classNamePrefix="custom"
-                                onChange={(selected) => {
-                                    field.onChange(selected.map((option) => option.value)); // Map selected options to their values
-                                }}
-                                value={options.filter((option) => field.value?.includes(option.value))} // Sync selected options with form state
-                            />
-                        )}
-                    />
-                    {errors[name] && <p className="error-message"  >{errors[name]?.message}</p>}
-                </div>
-            </>
-        );
-    }
-*/
+export default function RHFSelect({ name, className, options, placeholder, label, isMultiSelect = false, isSearchable = false }) {
+    const {
+      control,
+      formState: { errors },
+    } = useFormContext(); // Access register and errors from the form context
+  
+    return (
+      <>
+        <div className="form-group">
+          <label className="input-label" for={name}>{label}</label>
+          <Controller
+            name={name}
+            control={control}
+            rules={{ required: "Goal is required" }}
+            render={({ field }) => (
+              <Select
+              isMulti={isMultiSelect} // No need for ternary operator, true/false will work directly
+              {...field}
+              className={className}
+              options={options}
+              placeholder={placeholder}
+              isSearchable={isSearchable} 
+              classNamePrefix="custom"
+              onChange={(selected) => {
+                if (isMultiSelect) {
+                  // For multi-select: map selected options to their values
+                  field.onChange(selected.map((option) => option.value));
+                } else {
+                  // For single-select: just return the selected value
+                  field.onChange(selected ? selected.value : null);
+                }
+              }}
+              value={isMultiSelect 
+                ? options.filter((option) => field.value?.includes(option.value)) // For multi-select
+                : options.find((option) => option.value === field.value) // For single-select
+              }
+              />
+            )}
+          />
+          {errors[name] && <p className="error-message"  >{errors[name]?.message}</p>}
+        </div>
+      </>
+    );
+  }
